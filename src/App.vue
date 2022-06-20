cf0078bc5ff0eec34d4dd72ea54111391fc054b9
Ernest-Galiullin <91132298+Ernest-Galiullin@users.noreply.github.com>
2022-06-20 15:29:31 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
